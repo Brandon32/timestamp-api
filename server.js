4754a4f178cc7fc09fd3b019d55091264c6ef663
Brandon <kem5brandon@gmail.com>
2016-12-01 18:56:41 +0000

    Add inital working app

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Timestamp API');
+});
+
+app.get('/*', function (req, res) {
+  var date = decodeURIComponent(req.originalUrl).slice(1);
+  var objDate = new Date();
+  if(date.split(' ').length === 1){
+    objDate = new Date();
+    objDate.setTime(date * 1000);
+  }
+  else{
+    objDate = new Date(date);
+  }
+  var natural = objDate.toLocaleString("en-us", { month: "long" }) + " ";
+  natural += objDate.getDate() + ', ';
+  natural += objDate.getFullYear();
+  var unix = Math.floor(objDate.getTime()/1000);
+  var converted = {
+    "unix":objDate.getTime()?unix:null,
+    "natural":objDate.getTime()?natural:null
+  };
+  console.log(objDate,converted.unix,converted.natural);
+  res.send(JSON.stringify(converted));
+});
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+  console.log('Timestamp API app listening on port 8080!');
 })
